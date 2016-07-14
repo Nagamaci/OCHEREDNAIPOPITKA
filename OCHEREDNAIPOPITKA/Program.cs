8390793717002581b9f98b2b2521fce9293a9969
Vegitto <Vegitto@DESKTOP-8OF54ME>
2016-07-15 01:12:51 +0300

    Add project files.

---
+++ OCHEREDNAIPOPITKA/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCHEREDNAIPOPITKA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
